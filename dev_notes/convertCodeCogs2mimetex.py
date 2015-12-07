745bb26eafcd2f6952ddb35c88c525f2cd84b397
qibinyi <qibinyi@peoplemediumdeMac-mini-3.local>
2015-12-07 11:46:14 +0800

    m

---
+++ dev_notes/convertCodeCogs2mimetex.py
@@ -0,0 +1,44 @@
+
+#coding:utf8
+
+import os, sys
+import re
+
+RE_PATTERN_CODECOG= re.compile( r"(!\s*\[\s*\]\s*\(\s*http://latex\.codecogs\.com/gif\.latex\?)(.*?)(\))" )
+RE_PATTERN_ESCAPE = re.compile( r"&\w+;" )
+
+def visit(arg, dirname, names):
+	for name in names:
+		if name[-3:] == '.md':
+			#print name
+			try:
+				path = os.path.join( dirname , name )
+				fp = open(path)
+				content = fp.read()
+				fp.close()
+
+				
+				#results = re.findall( RE_PATTERN_CODECOG , content  )
+				"""
+				for result in results:
+					#print result[1]
+					print re.findall( RE_PATTERN_ESCAPE ,  result[1] )
+				#"""
+
+				content = re.gsub( RE_PATTERN_CODECOG ,  "http://www.forkosh.com/cgi-bin/mimetex.cgi?"  + r'\2\3'   , content )
+				content = re.gsub( RE_PATTERN_ESCAPE ,   '+'  , content )
+
+				fp = codecs.open( path  , "w", "utf-8")
+            	fp.write(content )
+            	fp.close()
+
+			except:
+				import traceback
+				traceback.print_exc() 
+
+if __name__=='__main__':
+
+	os.path.walk( ".", visit, None )
+
+	print 'done'
+
