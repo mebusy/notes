3f1611fa3bcc1360de85332bb0f913ce3aea5f15
qibinyi <qibinyi@peoplemediumdeMac-mini-3.local>
2015-12-31 12:46:37 +0800

    m

---
+++ codes/algorithm.py
@@ -0,0 +1,54 @@
+#coding:utf8
+
+
+def MergeSort(lists):
+    if len(lists) <= 1:  # exit condition
+        return lists
+    num = int( len(lists)/2 ) # (1)
+    left = MergeSort(lists[:num])  #(2)
+    right = MergeSort(lists[num:]) #(2)
+    return Merge(left, right)  #(3)
+
+def Merge(left,right):
+    r, l=0, 0
+    reslut=[]
+    while l<len(left) and r<len(right):
+        if left[l] < right[r]:
+            reslut.append(left[l])
+            l += 1
+        else:
+            reslut.append(right[r])
+            r += 1
+    reslut += right[r:]
+    reslut += left[l:]
+    return reslut
+
+
+
+import unittest  
+import numpy as np
+
+class mytest(unittest.TestCase):  
+      
+    ##初始化工作  
+    def setUp(self):  
+        pass  
+      
+    #退出清理工作  
+    def tearDown(self):  
+        pass  
+      
+    #具体的测试用例，一定要以test开头  
+    def testMergeSort(self):  
+    	for i in xrange(100):
+    		size = np.random.randint( 10,30 )
+    		lists = list( np.random.randint(0, size,size ) ) #转成 普通list
+        	self.assertEqual( cmp( MergeSort(lists) , sorted(lists)  ) , 0, 'test MergeSort fail')  
+          
+          
+
+
+
+if __name__=='__main__':
+	unittest.main()  
+	
